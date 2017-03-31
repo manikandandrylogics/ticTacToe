93d4b78eb65d5730e10360f13a6c71f7734977cd
manikandandrylogics <manikandan@drylogics.com>
2017-03-31 19:17:27 +0530

    tictactoe started

---
+++ spec/ticTacToe_spec.rb
@@ -0,0 +1,42 @@
+
+describe 'TicTacToe' do 
+
+  it 'should have 2 players' do
+    
+  end
+
+
+  it 'should have unique symbols for each player' do
+
+  end
+
+  it 'should allow players to place their symbols' do
+
+  end
+
+  it 'should allow them to place alternatively' do
+
+
+  end
+
+  context 'win' do
+
+    it 'should declare win when same symbol is placed vertically' do
+
+    end 
+    it 'should declare win when same symbol is placed horizontally' do
+
+    end 
+    it 'should declare win when same symbol is placed diagonally' do
+
+    end 
+  end
+  
+  context 'draw' do
+
+    it 'should declare draw when all positions are occupied and there is no winner' do
+
+    end
+  end
+
+end
